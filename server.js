de75daef43771d3e8f84adb5f8bb023d5bf6fcf8
ADMIN\yhsieh2 <tony.ychsieh@gmail.com>
2018-04-22 21:10:16 -0400

    modify python directory name

--- server.js
+++ server.js
@@ -8,22 +8,27 @@ ws.on('connection', function(w){
     w.on('message', function(msg){
         var receieveObj = JSON.parse(msg);
 
-        console.log('Receive object from client: ' + receieveObj.func);
+        console.log('1Receive object from client: ' + receieveObj.func);
 
         if(receieveObj.func == "search_courses") {
             var options = {
                 args: [msg]
             };
-        
-            PythonShell.run('pythonFile/main.py', options, function (err, results) {
-                
+            console.log('2Receive object from client: ' + receieveObj.func);
+
+            PythonShell.run('pythonFiles/main.py', options, function (err, results) {
+                console.log('3Receive object from client: ' + receieveObj.func);
+  
                 var data = JSON.parse(results); //string to json
                 //data = JSON.stringify(data);
-
+                console.log('4Receive object from client: ' + receieveObj.func);
+  
                 var res = {
                     'func': receieveObj.func,
                     'data': data
                 }
+                console.log('5Receive object from client: ' + receieveObj.func);
+  
                 var res = JSON.stringify(res); // json to string
                 console.log('ready to send');
                 if(!(w.readyState === w.CLOSED))
